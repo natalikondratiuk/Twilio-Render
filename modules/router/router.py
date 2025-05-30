fb40ad69ca82c141c0cef3660c8df0a5a4bd2422
Nataliia Kondratiuk <71512255+natalikondratiuk@users.noreply.github.com>
2025-05-30 16:19:32 +0300

    Add voice route to main API

--- modules/router/router.py
+++ modules/router/router.py
@@ -1,13 +1,13 @@
 from __future__ import annotations
 
-from typing import Annotated
-from fastapi import FastAPI, APIRouter, Query
+from typing import Annotated, Union
+from fastapi import FastAPI, APIRouter, Query, Response, status
 
 from modules.worker import Worker
 from objects.configs import RouterParameters
 
 from objects.schemas import Recipient
-from objects.responses import SmsResponse
+from objects.responses import SmsResponse, CallResponse, FailedResponse
 
 class Controller:
     _WORKER: Worker
@@ -29,12 +29,26 @@ class Controller:
     @property
     def add_route(self) -> Controller:
         self._router.add_api_route(self._config.sms_params.route, self._send_sms, methods=[self._config.sms_params.methods],
-                                   response_model=SmsResponse)
+                                   response_model=Union[SmsResponse, FailedResponse])
+        self._router.add_api_route(self._config.voice_params.route, self._make_call,
+                                   methods=[self._config.voice_params.methods],
+                                   response_model=Union[CallResponse, FailedResponse])
+
         self._application.include_router(self._router)
 
         return self
 
-    async def _send_sms(self, recipient: Annotated[Recipient, Query()]) -> SmsResponse:
-        return await self._WORKER.send_sms(recipient)
+    async def _send_sms(self, recipient: Annotated[Recipient, Query()], response: Response):
+        try:
+            return await self._WORKER.send_sms(recipient)
+        except Exception as e:
+            response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+            return FailedResponse()
 
+    async def _make_call(self, recipient: Annotated[Recipient, Query()], response: Response):
+        try:
+            return await self._WORKER.make_call(recipient)
+        except Exception as e:
+            response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+            return FailedResponse()
 
